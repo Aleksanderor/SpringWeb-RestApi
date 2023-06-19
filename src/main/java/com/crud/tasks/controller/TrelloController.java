01767ee55e35aaa2867e472d953af27162023af1
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-06-19 19:09:47 +0200

    kodilla boards filter added, name in app prop, trelloController edited

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,30 +3,27 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/trello")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @CrossOrigin("*")
 public class TrelloController {
 
     private final TrelloClient trelloClient;
 
-    @GetMapping("boards")
-    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+    @Autowired
+    public TrelloController(TrelloClient trelloClient) {
+        this.trelloClient = trelloClient;
+    }
 
-        trelloBoards.stream()
-                .filter(board -> board.getId() != null && board.getName() != null)
-                .filter(board -> board.getName().contains("Kodilla"))
-                .forEach(board -> {
-                    System.out.println(board.getId() + " " + board.getName());
-                });
+    @GetMapping("/boards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 }
--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -25,29 +26,27 @@ public class TrelloClient {
     private String trelloAppKey;
     @Value("${trello.app.token}")
     private String trelloToken;
+
     @Value("${trello.username}")
-    private String trelloUsername;
+    private String trelloUser;
 
     public List<TrelloBoardDto> getTrelloBoards() {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/kodillaautor/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id")
-                .build()
-                .encode()
-                .toUri();
+        URI url = buildUrl();
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
         return Optional.ofNullable(boardsResponse)
                 .map(Arrays::asList)
-                .orElse(Collections.emptyList());
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(board -> board.getId() != null && board.getName() != null)
+                .filter(board -> board.getName().contains("Kodilla"))
+                .collect(Collectors.toList());
     }
 
     private URI buildUrl() {
-        String apiUrl = String.format("%s/members/%s/boards", trelloApiEndpoint, trelloUsername);
-
-        return UriComponentsBuilder.fromHttpUrl(apiUrl)
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint)
+                .pathSegment("members", trelloUser, "boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id")
