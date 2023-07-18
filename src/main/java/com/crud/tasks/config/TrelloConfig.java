97297c8c9159b03679c88e3cbb14bb5fe8960893
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-07-18 21:15:06 +0200

    task 22.3 & 22.4

---
+++ src/main/java/com/crud/tasks/config/TrelloConfig.java
@@ -0,0 +1,23 @@
+package com.crud.tasks.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class TrelloConfig {
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+    @Value("${trello.app.user}")
+    private String trelloUser;
+}
+
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,9 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,19 +14,21 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/trello")
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @CrossOrigin("*")
 public class TrelloController {
 
     private final TrelloClient trelloClient;
 
-    @Autowired
-    public TrelloController(TrelloClient trelloClient) {
-        this.trelloClient = trelloClient;
+
+    @GetMapping("boards")
+    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoards(){
+        return ResponseEntity.ok(trelloClient.getTrelloBoards());
     }
 
-    @GetMapping("/boards")
-    public List<TrelloBoardDto> getTrelloBoards() {
-        return trelloClient.getTrelloBoards();
+    @PostMapping("cards")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
     }
+
 }
